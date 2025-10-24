65051b64a83e58ab0b23e616aece3d4bb919cbe1
James <james@ammortal.com>
2025-10-24 13:26:12 -0400

    add variance, std, z

--- node/anomaly.py
+++ node/anomaly.py
@@ -3,3 +3,22 @@ class ZScore:
         self.n = 0
         self.mean = 0.0
         self.M2 = 0.0
+
+    def update(self, x: float):
+        self.n += 1
+        delta = x - self.mean
+        self.mean += delta / self.n
+        delta2 = x - self.mean
+        self.M2 += delta * delta2
+
+    @property
+    def variance(self):
+        return self.M2 / (self.n - 1) if self.n > 1 else 0.0
+
+    @property
+    def std(self):
+        return self.variance ** 0.5
+
+    def z(self, x: float) -> float:
+        s = self.std
+        return (x - self.mean) / s if s > 1e-9 else 0.0
